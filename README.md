0cba107bf1e4a7e8c8dc80865cec7abb0ca14b6e
Vanessa MotaLi <lijiayo@os1.engr.oregonstate.edu>
2024-03-08 09:44:06 -0800

    finished

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # group18
+
+Citation:
+
+https://randomuser.me/ (10/23/2023) For users sample data in DDL.sql
+
+@mlapresta (11/07/2023) Citing source code flask-starter-app https://github.com/osu-cs340-ecampus/flask-starter-app
--- app.py
+++ app.py
@@ -3,17 +3,29 @@ from flask_mysqldb import MySQL
 from flask import request
 import os
 import database.db_connector as db
+from database.db_credentials import host, user, passwd, db
+from database.db_connector import connect_to_database, execute_query
+
+# Citation Scope: Flask and database connection set up on filp and each CRUD operation
+# Date: 11/07/2023
+# Originality: Adapted
+# Source: https://github.com/osu-cs340-ecampus/flask-starter-app
 
 
-
-db_connection = db.connect_to_database()
+#db_connection = db.connect_to_database()
 
 # Configuration
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = host
+app.config['MYSQL_USER'] = user
+app.config['MYSQL_PASSWORD'] = passwd
+app.config['MYSQL_DB'] = db
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 mysql = MySQL(app)
+
 
 # Routes 
 
@@ -22,7 +34,7 @@ def root():
     return render_template("main.j2")
 
 ########## Functions for Users page, read/create/update/delete functions#################
-#### Read and Create operation
+#### Read and Create operation for users
 @app.route('/users', methods=['POST','GET'])
 def users():
     if request.method == "GET":
@@ -60,7 +72,7 @@ def search_user():
         else:
             return render_template("founduser.j2", message = 'User not found.')
 
-#### Update operation
+#### Update operation for users
 @app.route('/edit_user/<int:ID>', methods=['POST','GET'])
 def edit_user(ID):
     if request.method == "GET":
@@ -85,7 +97,7 @@ def edit_user(ID):
         mysql.connection.commit()
         return redirect("/users")
 
-#### Delete operation
+#### Delete operation for users
 @app.route('/delete_user/<int:ID>')
 def delete_user(ID):
     query = "DELETE FROM Users WHERE user_id = '%s';"
@@ -98,7 +110,7 @@ def delete_user(ID):
 #############################################################
 
 ########## Functions for User Favorite page, read/create/update/delete functions#################
-#### Read and Create operation
+#### Read and Create operation for favorite
 @app.route('/favorite',  methods=['POST','GET'])
 def favorite():
     if request.method == "GET":
@@ -107,11 +119,17 @@ def favorite():
         cur.execute(query)
         data = cur.fetchall()
         
+        query1 = 'SELECT user_email FROM Users;'
+        cur = mysql.connection.cursor()
+        cur.execute(query1)
+        emails = cur.fetchall()
+
         query2 = 'Select routine_name From Routines;'
         cur = mysql.connection.cursor()
         cur.execute(query2)
         routines = cur.fetchall()
-        return render_template("favorite.j2", data=data, routines=routines)
+
+        return render_template("favorite.j2", data=data, emails=emails, routines=routines)
 
     if request.method == "POST":
         if request.form.get("Create_Log"):
@@ -131,7 +149,7 @@ def favorite():
         mysql.connection.commit()
         return redirect("/favorite")
 
-#### Update operation
+#### Update operation for favorite
 @app.route('/edit_log/<int:ID>', methods=['POST','GET'])
 def edit_log(ID):
     if request.method == "GET":
@@ -166,7 +184,7 @@ def edit_log(ID):
         mysql.connection.commit()
         return redirect("/favorite")
 
-#### Delete operation
+#### Delete operation for favorite
 @app.route('/delete_log/<int:ID>')
 def delete_log(ID):
     query = "DELETE FROM Favorited_routine_logs WHERE log_id = '%s';"
@@ -179,7 +197,7 @@ def delete_log(ID):
 #############################################################################################
 
 ########## Functions for Routines page, read/create/update/delete functions #################
-#### Read operation
+#### Read and Create operation
 @app.route('/routines', methods=['POST','GET'])
 def routines():
     if request.method == "GET":
@@ -187,8 +205,83 @@ def routines():
         cur = mysql.connection.cursor()
         cur.execute(query)
         data = cur.fetchall()
-        
-        return render_template("routines.j2", data=data)
+
+        query1 = 'SELECT equipment_id as ID, equipment_name FROM Equipment;'
+        cur = mysql.connection.cursor()
+        cur.execute(query1)
+        equipments = cur.fetchall()
+
+        query2 = 'SELECT routine_category_id as ID, routine_category_name From Routine_categories;'
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        categories = cur.fetchall()
+
+        return render_template("routines.j2", data=data, equipments=equipments, categories=categories)
+    
+    if request.method == "POST":
+        if request.form.get("Create_Routine"):
+            routine_name = request.form["routine"]
+            description = request.form["description"]
+            duration = request.form["duration"]
+            video_link = request.form["video"]
+            equipment = request.form["equipment"]
+            category = request.form["category"]
+            
+        if equipment == "":
+            query = "INSERT INTO Routines (routine_name, description, duration, video_link, routine_category_id) VALUES (%s, %s, %s, %s, %s);"
+            cur = mysql.connection.cursor()
+            cur.execute(query,(routine_name, description, duration, video_link, category))
+            mysql.connection.commit()
+            
+        else:
+            query = "INSERT INTO Routines (routine_name, description, duration, video_link, equipment_id, routine_category_id) VALUES (%s, %s, %s, %s, %s, %s);"
+            cur = mysql.connection.cursor()
+            cur.execute(query,(routine_name, description, duration, video_link, equipment, category))
+            mysql.connection.commit()
+        return redirect("/routines")
+
+#### Update operation for Routines
+@app.route('/edit_routine/<int:ID>', methods=['POST','GET'])
+def edit_routine(ID):
+    if request.method == "GET":
+        query = "SELECT routine_id, routine_name, description, duration as Duration, video_link, IFNULL(Equipment.equipment_name, 'None'), Routine_categories.routine_category_name as Category From Routines JOIN Routine_categories ON Routines.routine_category_id = Routine_categories.routine_category_id LEFT JOIN Equipment ON Routines.equipment_id = Equipment.equipment_id WHERE Routine_id = %s;"
+        cur = mysql.connection.cursor()
+        cur.execute(query,(ID,))
+        data=cur.fetchall()
+
+        query1 = 'SELECT equipment_id as ID, equipment_name FROM Equipment;'
+        cur = mysql.connection.cursor()
+        cur.execute(query1)
+        equipments = cur.fetchall()
+
+        query2 = 'SELECT routine_category_id as ID, routine_category_name From Routine_categories;'
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        categories = cur.fetchall()
+        return render_template("edit_routine.j2", data=data, equipments=equipments, categories=categories)
+
+    if request.method == "POST":
+        if request.form.get("Edit_Routine"):
+            routine_name = request.form["routine"]
+            description = request.form["description"]
+            duration = request.form["duration"]
+            video_link = request.form["video"]
+            equipment = request.form["equipment"]
+            category = request.form["category"]
+            routine_id = request.form["routineID"]
+            
+        if equipment == "":
+            query = "UPDATE Routines SET routine_name = %s, description = %s, duration = %s, video_link= %s, routine_category_id = %s WHERE routine_id = %s;"
+            cur = mysql.connection.cursor()
+            cur.execute(query,(routine_name, description, duration, video_link, category, routine_id))
+            mysql.connection.commit()
+            
+        else:
+            query = "UPDATE Routines SET routine_name = %s, description = %s, duration = %s, video_link= %s, equipment_id = %s, routine_category_id = %s WHERE routine_id = %s;"
+            cur = mysql.connection.cursor()
+            cur.execute(query,(routine_name, description, duration, video_link, equipment, category, routine_id))
+            mysql.connection.commit()
+        return redirect("/routines")
 
 #### Delete operation for Routines
 @app.route('/delete_routine/<int:ID>')
@@ -251,5 +344,5 @@ def equipment():
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 21295)) 
+    port = int(os.environ.get('PORT', 3891)) 
     app.run(port=port, debug=True)
